ac84209b2dd25f4e805a9214a482190ed3a6a4be
SidZhoop <siddharth@zhoop.in>
2023-03-27 01:13:44 +0530

    added context function for cart

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bfbdeb51777095a6"; }
+/******/ 		__webpack_require__.h = function() { return "772519413d484cde"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../../components/Layout";
 import data from "../../utils/data";
+import { Store } from "../../utils/Store";
 
 const ProductScreen = () => {
+  const { state, dispatch } = useContext(Store);
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
@@ -13,6 +15,18 @@ const ProductScreen = () => {
   if (!product) {
     return <div>Product Not Found!</div>;
   }
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    if (product.countInStock < quantity) {
+      alert("Sorry. Product is out of stock!");
+      return;
+    }
+
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
+  };
 
   return (
     <Layout title={product.name}>
@@ -54,7 +68,12 @@ const ProductScreen = () => {
                 {product.countInStock > 0 ? "In stock" : "Out of Stock"}
               </div>
             </div>
-            <button className="primary-button w-full">Add to cart</button>
+            <button
+              className="primary-button w-full"
+              onClick={addToCartHandler}
+            >
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
