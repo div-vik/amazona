6123ec5b55b57353730e0f0bf67ced2d7a4815d7
SidZhoop <siddharth@zhoop.in>
2023-03-26 20:51:55 +0530

    Added navbar and footer

---
+++ components/Layout.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+
+const Layout = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + " - Amazona" : "Amazona"}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 justify-between shadow-md items-center px-4">
+            <Link className="text-lg font-bold" href="/">
+              amazona
+            </Link>
+            <div>
+              <Link className="p-2" href="/cart">
+                Cart
+              </Link>
+              <Link className="p-2" href="/login">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+
+        <main className="container m-auto mt-4 px-4">{children}</main>
+
+        <footer className="flex justify-center items-center h-10 shadow-inner">
+          <p>Copyright © 2023 Amazona</p>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
